095adfa429e2310008e18128c72b65c728edc7e0
OrionAlzate <orion_alzate@hotmail.com>
2023-04-11 16:57:30 -0500

    colores footer and nvbar ready

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,4 +1,6 @@
 import './Footer.css'
+import { Link } from "react-router-dom"
+
 export function Footer() {
     return (
         <>
@@ -6,24 +8,32 @@ export function Footer() {
                 <div class="map_footer">
                     <div class="map_footer_container">
                         <h4>Mapa del Sitio</h4>
-                        <br/>
-                        <p>Home</p>
-                        <p>Agregar Productos</p>
-                        <p>Productos</p>
+                        <br />
+                        <ul class="">
+                            <li class="list_item_footer">
+                                <Link class="lista_footer" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li class="list_item_footer">
+                                <Link class="lista_footer" to="/administrar">Agregar Productos</Link>
+                            </li>
+                            <li class="list_item_footer">
+                                <Link class="lista_footer" to="/productos">Productos</Link>
+                            </li>
+                        </ul>
                     </div>
 
                 </div>
                 <div class="contact_footer">
                     <h4>Contáctenos</h4>
-                    <br/>
+                    <br />
                     <p>Mail: moraazul@mimail.com</p>
                     <p>Tel: +57 321 6549877</p>
                 </div>
                 <div class="redes_footer">
                     <h4>Redes Sociales</h4>
-                    <br/>
+                    <br />
                     <div class="redes_icons">
-                        <i class="bi bi-facebook"></i>     
+                        <i class="bi bi-facebook"></i>
                         <i class="bi bi-instagram"></i>
                         <i class="bi bi-tiktok"></i>
                         <i class="bi bi-twitter"></i>
--- src/Servicios/Servicios.js
+++ src/Servicios/Servicios.js
@@ -5,7 +5,7 @@ export function Servicios() {
         <>
             <section class="container-fluid my-5">
                 <div class="row text-center justify-content-around">
-                    <div class="col-12 col-md-3 fondo">
+                    <div class="col-12 col-lg-3  fondo">
 
                         <h2 class="titulo_servicios">Compra desde nuestro sitio web</h2>
                         <i class="bi bi-cash-coin fs-1 icon"></i>
@@ -13,7 +13,7 @@ export function Servicios() {
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat tempore nesciunt dignissimos sunt quasi, fugit, consequuntur asperiores suscipit, optio aliquam perferendis in laboriosam sint beatae architecto aspernatur fugiat! Harum, magnam.
                         </p>
                     </div>
-                    <div class="col-12 col-md-3 fondo">
+                    <div class="col-12 col-lg-3  fondo">
                         <h2 class="titulo_servicios">Envíos a todo el país</h2>
 
                         <i class="bi bi-box-seam fs-1 icon"></i>
@@ -21,7 +21,7 @@ export function Servicios() {
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat tempore nesciunt dignissimos sunt quasi, fugit, consequuntur asperiores suscipit, optio aliquam perferendis in laboriosam sint beatae architecto aspernatur fugiat! Harum, magnam.
                         </p>
                     </div>
-                    <div class="col-12 col-md-3 fondo">
+                    <div class="col-12 col-lg-3  fondo">
                         <h2 class="titulo_servicios">Paga con cualquier método de pago</h2>
                         <i class="bi bi-credit-card fs-1 icon"></i>
                         <p class="texto_servicios">
